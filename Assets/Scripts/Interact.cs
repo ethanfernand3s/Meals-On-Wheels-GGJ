1cce54a47dbdc6469ab3c8daaecab642043822c0
Dylan Fernandes <dfernandes4@student.cccd.edu>
2025-04-16 23:09:45 -0700

    Setup up the ability to pickup, drop, and throw an item, and started dialogue (WIP)

---
+++ Assets/Scripts/Interact.cs
@@ -0,0 +1,40 @@
+using System;
+using DefaultNamespace;
+using UnityEngine;
+
+public class Interact : MonoBehaviour
+{
+    [Header("Interaction Settings")]
+    public float interactRange = 3f;
+    public KeyCode interactKey = KeyCode.E;
+    public LayerMask interactLayer;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(interactKey))
+        {
+            TryInteract();
+        }
+    }
+
+    private void TryInteract()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        Debug.DrawRay(ray.origin, ray.direction * interactRange, Color.red);
+
+        // Use the interactLayer in the Raycast
+        if (!Physics.Raycast(ray, out RaycastHit hit, interactRange, interactLayer)) return;
+
+        GameObject hitObject = hit.collider.gameObject;
+
+        IInteractable interactable = hitObject.GetComponent<IInteractable>();
+        if (interactable != null)
+        {
+            interactable.Interact(hit);
+        }
+        else
+        {
+            Debug.LogWarning($"'{hitObject.name}' Can't Interact With Object Since There Is No Interactable Interface");
+        }
+    }
+}
---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,120 @@
+using DefaultNamespace;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour, IInteractable
+{
+    public GameObject player;
+    public Transform holdPos;
+    
+    public float throwForce = 500f; //force at which the object is thrown at
+    private float rotationSensitivity = 1f; //how fast/slow the object is rotated in relation to mouse movement
+    private GameObject heldObj; //object which we pick up
+    private Rigidbody heldObjRb; //rigidbody of object we pick up
+    private bool canDrop = true; //this is needed so we don't throw/drop object when rotating the object
+    private int LayerNumber; //layer index
+    
+    void Start()
+    {
+        LayerNumber = LayerMask.NameToLayer("holdLayer"); 
+    }
+
+    public void Interact(RaycastHit hit)
+    {
+        if (heldObj == null) //if currently not holding anything
+        {
+            //make sure pickup tag is attached
+            if (hit.transform.gameObject.tag == "canPickUp")
+            {
+                //pass in object hit into the PickUpObject function
+                PickUpObject(hit.transform.gameObject);
+            }
+        }
+        else
+        {
+            if (canDrop)
+            {
+                StopClipping(); //prevents object from clipping through walls
+                DropObject();
+            }
+        }
+    }
+    void Update()
+    {
+        if (heldObj != null) //if player is holding object
+        {
+            MoveObject(); //keep object position at holdPos
+            RotateObject();
+            if (Input.GetKeyDown(KeyCode.Mouse0) && canDrop)
+            {
+                StopClipping();
+                ThrowObject();
+            }
+        }
+    }
+    void PickUpObject(GameObject pickUpObj)
+    {
+        if (pickUpObj.GetComponent<Rigidbody>()) //make sure the object has a RigidBody
+        {
+            heldObj = pickUpObj; //assign heldObj to the object that was hit by the raycast (no longer == null)
+            heldObjRb = pickUpObj.GetComponent<Rigidbody>(); //assign Rigidbody
+            heldObjRb.isKinematic = true;
+            heldObjRb.transform.parent = holdPos.transform; //parent object to holdposition
+            heldObj.layer = LayerNumber; //change the object layer to the holdLayer
+            //make sure object doesnt collide with player, it can cause weird bugs
+            Physics.IgnoreCollision(heldObj.GetComponent<Collider>(), player.GetComponent<Collider>(), true);
+        }
+    }
+    void DropObject()
+    {
+        //re-enable collision with player
+        Physics.IgnoreCollision(heldObj.GetComponent<Collider>(), player.GetComponent<Collider>(), false);
+        heldObj.layer = 3; //object assigned back to default layer
+        heldObjRb.isKinematic = false;
+        heldObj.transform.parent = null; //unparent object
+        heldObj = null; //undefine game object
+    }
+    void MoveObject()
+    {
+        //keep object position the same as the holdPosition position
+        heldObj.transform.position = holdPos.transform.position;
+    }
+    void RotateObject()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            canDrop = false; //make sure throwing can't occur during rotating
+
+            Vector3 rightRotation = Vector3.right;
+            heldObj.transform.Rotate(rightRotation * rotationSensitivity);
+        }
+        else
+        {
+            canDrop = true;
+        }
+    }
+    void ThrowObject()
+    {
+        //same as drop function, but add force to object before undefining it
+        Physics.IgnoreCollision(heldObj.GetComponent<Collider>(), player.GetComponent<Collider>(), false);
+        heldObj.layer = 3;
+        heldObjRb.isKinematic = false;
+        heldObj.transform.parent = null;
+        heldObjRb.AddForce(transform.forward * throwForce);
+        heldObj = null;
+    }
+    void StopClipping() //function only called when dropping/throwing
+    {
+        var clipRange = Vector3.Distance(heldObj.transform.position, transform.position); //distance from holdPos to the camera
+        //have to use RaycastAll as object blocks raycast in center screen
+        //RaycastAll returns array of all colliders hit within the cliprange
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(transform.position, transform.TransformDirection(Vector3.forward), clipRange);
+        //if the array length is greater than 1, meaning it has hit more than just the object we are carrying
+        if (hits.Length > 1)
+        {
+            //change object position to camera position 
+            heldObj.transform.position = transform.position + new Vector3(0f, -0.5f, 0f); //offset slightly downward to stop object dropping above player 
+            //if your player is small, change the -0.5f to a smaller number (in magnitude) ie: -0.1f
+        }
+    }
+}
